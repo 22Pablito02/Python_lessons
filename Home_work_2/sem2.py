e3978cab73dfe44065dc4ee05315a0ce9127e48a
Павел Волков <PavelVolkovPro@yandex.ru>
2022-12-03 14:50:18 +0300

    task3

--- Home_work_2/sem2.py
+++ Home_work_2/sem2.py
@@ -12,15 +12,27 @@
 
 # print(sum)
 
-#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-print("Введите число: ", end = "")
-num = int(input(""))
-sum = 1
-lol = []
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# print("Введите число: ", end = "")
+# num = int(input(""))
+# sum = 1
+# lol = []
 
-for i in range(1,num + 1):
-    sum = 1
-    for j in range(1,i + 1):
-        sum = sum * j
-    lol.append(sum)
-print(*lol, sep = ", ")
+# for i in range(1,num + 1):
+#     sum = 1
+#     for j in range(1,i + 1):
+#         sum = sum * j
+#     lol.append(sum)
+# print(*lol, sep = ", ")
+
+# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+import random
+n = random.randint(2,10)
+sheet = []
+sum = 0
+for i in range(1,n):
+    sum =   round((1 + 1 / i) ** i, 2)
+    sheet.append(sum)
+print(*sheet, sep = ", ")
+
