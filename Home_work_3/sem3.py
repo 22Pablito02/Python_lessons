93a5df0460c8d1c4e8ecd10584129854680dece4
Pablito <PavelVolkovPro@yandex.ru>
2022-12-05 12:58:40 +0300

    task3

--- Home_work_3/sem3.py
+++ Home_work_3/sem3.py
@@ -13,11 +13,13 @@ def FillList(size):
 def PrintList(sheet):
     print(*sheet, sep = ", ")
 
+# sheet = FillList(ListSize())
+# PrintList(sheet)
+
 
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
-sheet = FillList(ListSize())
-PrintList(sheet)
+
 
 # def SumOdd(RList):
 #     sum = 0
@@ -31,18 +33,34 @@ PrintList(sheet)
 # Напишите программу, которая найдёт произведение пар чисел списка. 
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-ResList = []
-lenList = len(sheet)
+# ResList = []
+# lenList = len(sheet)
 
-if lenList % 2 != 0:
-    lenList = lenList + 1
-for i in range(0, lenList // 2):
-    if i == lenList // 2:  
-        ResList.append(sheet[i] * sheet[i])
-    else:
-        ResList.append(sheet[i] * sheet[-i-1])
+# if lenList % 2 != 0:
+#     lenList = lenList + 1
+# for i in range(0, lenList // 2):
+#     if i == lenList // 2:  
+#         ResList.append(sheet[i] * sheet[i])
+#     else:
+#         ResList.append(sheet[i] * sheet[-i-1])
+# PrintList(ResList)
 
 
 
+# Задайте список из вещественных чисел. 
+# Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
-PrintList(ResList)
+
+def FillListFloat(size):
+    list = []
+    for i in range(0, size):
+        list.append(round(random.uniform(0, 10), 2))
+    return list
+
+
+sheet = FillListFloat(ListSize())
+PrintList(sheet)
+for i in range(0, len(sheet)):
+    sheet[i] = round(sheet[i] - int(sheet[i]), 2)
+PrintList(sheet)
+print(max(sheet) - min(sheet)) 
