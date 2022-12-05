69e8d38c630b4970279738a8a18c51efc3e96f39
Pablito <PavelVolkovPro@yandex.ru>
2022-12-05 12:28:09 +0300

    task2

--- Home_work_3/sem3.py
+++ Home_work_3/sem3.py
@@ -1,14 +1,13 @@
 import random
+import math
 
 def ListSize():
-    print("Введите размер списка:", end = " ")
-    return int(input(""))
+    return int(input("Введите размер списка: "))
 
 def FillList(size):
     list = []
     for i in range(0, size):
-        temp = random.randint(0, 10)
-        list.append(temp)
+        list.append(random.randint(0, 10))
     return list
 
 def PrintList(sheet):
@@ -20,14 +19,30 @@ def PrintList(sheet):
 sheet = FillList(ListSize())
 PrintList(sheet)
 
-def SumOdd(RList):
-    sum = 0
-    for i in range(0, len(RList)):
-        if i % 2 != 0:
-            sum += RList[i]
-    print(sum)
+# def SumOdd(RList):
+#     sum = 0
+#     for i in range(0, len(RList)):
+#         if i % 2 != 0:
+#             sum += RList[i]
+#     print(sum)
 
-SumOdd(sheet)
+# SumOdd(sheet)
+
+# Напишите программу, которая найдёт произведение пар чисел списка. 
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+ResList = []
+lenList = len(sheet)
+
+if lenList % 2 != 0:
+    lenList = lenList + 1
+for i in range(0, lenList // 2):
+    if i == lenList // 2:  
+        ResList.append(sheet[i] * sheet[i])
+    else:
+        ResList.append(sheet[i] * sheet[-i-1])
 
 
 
+
+PrintList(ResList)
