532936de4f0eb516ee719f602c811c44317f9c72
Павел Волков <PavelVolkovPro@yandex.ru>
2022-12-08 15:15:46 +0300

    task3

--- Home_work_4/sem4.py
+++ Home_work_4/sem4.py
@@ -16,16 +16,27 @@ def PrintList(sheet):
 # 2 Задайте натуральное число N. 
 # Напишите программу, которая составит список простых множителей числа N.
 
-n = int(input("Введите число: "))
-res = []
+# n = int(input("Введите число: "))
+# res = []
 
-while n != 1:
-    i = 2
-    while 1:
-        if n % i == 0:
-            n = n / i
-            res.append(i)
-            break
-        else:
-            i += 1
-print (*res, sep = ", ")
+# while n != 1:
+#     i = 2
+#     while 1:
+#         if n % i == 0:
+#             n = n / i
+#             res.append(i)
+#             break
+#         else:
+#             i += 1
+# print (*res, sep = ", ")
+
+# Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
+
+
+sheet = [1,2,3,6,45,6,32,4,4,2,1]
+res = []
+for i in range(0, len(sheet)):
+    if sheet.count(sheet[i]) == 1:
+        res.append(sheet[i])
+PrintList(res)
