83738a45831247459cb2c823cc0902e067b8b999
Pablito <PavelVolkovPro@yandex.ru>
2022-12-16 15:00:16 +0300

    task4

--- Home_work_5/sem5.py
+++ Home_work_5/sem5.py
@@ -79,79 +79,141 @@ import math
 
 # 3 Создайте программу для игры в "Крестики-нолики"
 
-from collections import Counter
+# from collections import Counter
 
-arrView = [['1', '2', '3'],['4', '5', '6'],['7', '8', '9']]
-# print(*arrView, sep = "\n")
-arrWork = [['-', '-', '-'],['-', '-', '-'],['-', '-', '-']]
-# print(*arrWork, sep = "\n")
-finish = 0
+# arrView = [['1', '2', '3'],['4', '5', '6'],['7', '8', '9']]
+# # print(*arrView, sep = "\n")
+# arrWork = [['-', '-', '-'],['-', '-', '-'],['-', '-', '-']]
+# # print(*arrWork, sep = "\n")
+# finish = 0
 
-def PrintMatrix():
-    print("\n")
-    print(*arrView, sep = "\n")
-    print()
-    print(*arrWork, sep = "\n")
-    print()
+# def PrintMatrix():
+#     print("\n")
+#     print(*arrView, sep = "\n")
+#     print()
+#     print(*arrWork, sep = "\n")
+#     print()
 
-def GetValue(token):
-    UntiDuble = 0
-    ElNum = input(f"Введите цифру поля, куда хотите поставить {token}: ")
-    while UntiDuble != 1 or ElNum == "":
-        for i in range(0, len(arrView)):
-            if ElNum in arrView[i]: UntiDuble += 1
-        if ElNum == "": 
-            ElNum = input(f"Вы не выбрали цыфру!\nВведите цифру поля, куда хотите поставить {token}: ")
-        elif UntiDuble == 0:
-            ElNum = input(f"Такой цыфры нет!\nВведите цифру поля, куда хотите поставить {token}: ")
-    return ElNum
+# def GetValue(token):
+#     UntiDuble = 0
+#     ElNum = input(f"Введите цифру поля, куда хотите поставить {token}: ")
+#     while UntiDuble != 1 or ElNum == "":
+#         for i in range(0, len(arrView)):
+#             if ElNum in arrView[i]: UntiDuble += 1
+#         if ElNum == "": 
+#             ElNum = input(f"Вы не выбрали цыфру!\nВведите цифру поля, куда хотите поставить {token}: ")
+#         elif UntiDuble == 0:
+#             ElNum = input(f"Такой цыфры нет!\nВведите цифру поля, куда хотите поставить {token}: ")
+#     return ElNum
 
-def SettingValue(token):
-    PrintMatrix()
-    key = GetValue(token)
-    for i in range(0, len(arrView)):
-        for j in range(0, len(arrView[i])): 
-            if arrView[i][j] == key:
-                arrView[i][j] = " " 
-                arrWork[i][j] = token
-
-
-def VictoryCheck(finish):
-    for i in range(0, len(arrWork)):
-        if arrWork[i].count("X") == 3 or arrWork[i].count("O") == 3:
-            PrintMatrix()
-            print("Победа!")
-            finish = 1
-            break
-
-    for j in range(0, len(arrWork)):
-        Xsum = 0
-        Osum = 0
-        for i in range(0, 3):
-            if arrWork[i][j] == "X":
-                Xsum += 1
-            elif arrWork[i][j] == "O":
-                Osum += 1
-
-        if Xsum == 3 or Osum == 3:
-            PrintMatrix()
-            print("Победа!")
-            finish = 1
-            break
-
-    if  arrWork[0][0] == arrWork[1][1] == arrWork[2][2] != "-" or arrWork[0][2] == arrWork[1][1] == arrWork[2][0] != "-":
-        PrintMatrix()
-        print("Победа!")
-        finish = 1
-    return finish
+# def SettingValue(token):
+#     PrintMatrix()
+#     key = GetValue(token)
+#     for i in range(0, len(arrView)):
+#         for j in range(0, len(arrView[i])): 
+#             if arrView[i][j] == key:
+#                 arrView[i][j] = " " 
+#                 arrWork[i][j] = token
 
 
-while 1:
-    SettingValue("O")
-    finish = VictoryCheck(finish)
-    if finish == 1:
-        break
-    SettingValue("X")
-    finish = VictoryCheck(finish)
-    if finish == 1:
-        break
+# def VictoryCheck(finish):
+#     for i in range(0, len(arrWork)):
+#         if arrWork[i].count("X") == 3 or arrWork[i].count("O") == 3:
+#             PrintMatrix()
+#             print("Победа!")
+#             finish = 1
+#             break
+
+#     for j in range(0, len(arrWork)):
+#         Xsum = 0
+#         Osum = 0
+#         for i in range(0, 3):
+#             if arrWork[i][j] == "X":
+#                 Xsum += 1
+#             elif arrWork[i][j] == "O":
+#                 Osum += 1
+
+#         if Xsum == 3 or Osum == 3:
+#             PrintMatrix()
+#             print("Победа!")
+#             finish = 1
+#             break
+
+#     if  arrWork[0][0] == arrWork[1][1] == arrWork[2][2] != "-" or arrWork[0][2] == arrWork[1][1] == arrWork[2][0] != "-":
+#         PrintMatrix()
+#         print("Победа!")
+#         finish = 1
+#     return finish
+
+
+# while 1:
+#     SettingValue("O")
+#     finish = VictoryCheck(finish)
+#     if finish == 1:
+#         break
+#     SettingValue("X")
+#     finish = VictoryCheck(finish)
+#     if finish == 1:
+#         break
+
+
+# 4 Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+import os, sys
+def ReadingFile():
+    FileName = input("Введите имя файла для прочтения: ")
+    content = None
+    with open(os.path.join(os.path.dirname(sys.argv[0]),FileName),"r") as file:
+        return file.read(content)
+
+def WreatingFile(content):
+    FileName = input("Введите имя файла для записи: ")
+    with open(os.path.join(os.path.dirname(sys.argv[0]),FileName),"w") as file:
+        file.write(content)
+
+def coder(sheet):
+    res = []
+    count = 1
+    el = ""
+    for i in range(0,len(sheet)):
+        if i == len(sheet) - 1:
+            res.append(count)
+            res.append(sheet[i])
+        elif sheet[i] == sheet[i+1] and i != len(sheet) - 1:
+            count += 1
+            el = sheet[i]
+        elif sheet[i] != sheet[i+1]:
+            res.append(count)
+            res.append(sheet[i])
+            count = 1
+            el = ""
+            
+    res = str(res)
+    temp = ""
+    for i in range(0, len(res)): 
+        if res[i].isdigit() or res[i].isalpha():
+            temp += res[i]
+    return temp
+
+#Шифрование и запись
+code = ReadingFile().upper()
+# sheet = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"
+code = coder(code)
+print(code)
+WreatingFile(code)
+
+# Дешифрование
+def decoder(sheet):
+    temp = ""
+    num = ""
+    for i in range(0, len(sheet)): 
+        if sheet[i].isdigit():
+            num += sheet[i]
+        elif sheet[i].isalpha():
+            for j in range(0, int(num)):
+                temp += sheet[i]
+            num = ""
+    return temp
+
+code = decoder(ReadingFile().upper())
+print(code)
+
