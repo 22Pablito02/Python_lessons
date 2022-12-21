053d002cbebfe0b0fa99fc57dfacbcc3c18836e7
Павел Волков <PavelVolkovPro@yandex.ru>
2022-12-21 22:45:56 +0300

    task3.1

--- Home_work_6/sem6.py
+++ Home_work_6/sem6.py
@@ -17,27 +17,38 @@ from functools import reduce
 
 # 2 Напишите программу, которая подсчитывает наибольшее количество подряд выпавших Решек.
 
-def quantity_cast():
-    quantity = int(input("Введите кол-во бросков монетки: "))
-    sheet = ""
-    for i in range(0,quantity):
-        if rnd(1,2) == 1: 
-            sheet += "O" 
-        else:
-            sheet += "Р" 
-    print(sheet)
+# def quantity_cast():
+#     quantity = int(input("Введите кол-во бросков монетки: "))
+#     sheet = ""
+#     for i in range(0,quantity):
+#         if rnd(1,2) == 1: 
+#             sheet += "O" 
+#         else:
+#             sheet += "Р" 
+#     print(sheet)
+#     return sheet
+
+# def fiend_max_p(resSheet):
+#     count = 0
+#     temp = 0
+#     for i in range(0, len(resSheet)):
+#         if resSheet[i] != "Р":
+#             if temp < count:
+#                 temp = count
+#             count = 0
+#         elif resSheet[i] == "Р":
+#             count +=1
+#     print(temp)
+
+# fiend_max_p(quantity_cast())
+
+# 3.1 Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+def create_list():
+    sheet = []
+    num = int(input("Введите количество чисел: "))
+    for i in range(num):
+        sheet.append(rnd(0,10))
     return sheet
-
-def fiend_max_p(resSheet):
-    count = 0
-    temp = 0
-    for i in range(0, len(resSheet)):
-        if resSheet[i] != "Р":
-            if temp < count:
-                temp = count
-            count = 0
-        elif resSheet[i] == "Р":
-            count +=1
-    print(temp)
-
-fiend_max_p(quantity_cast())
+    
+print(reduce(lambda x,y: x+y, filter(lambda x: not x % 2, create_list())))
